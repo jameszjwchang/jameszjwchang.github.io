68e937b4af8667ac25bdc68d0b90ebfc54df8149
James Chang <jameszjwchang@outlook.com>
2025-03-12 00:17:09 +0800

    Fixed Position issues with front page

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741709335',resources: [
+    cacheName: 'chirpy-1741709760',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
